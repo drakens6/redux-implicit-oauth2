4e8680779ff258b7b63be0c823870a54655896b6
Drake Boley <drakens6666@gmail.com>
2019-04-12 14:59:52 -0700

    Update token.js

--- src/util/token.js
+++ src/util/token.js
@@ -1,29 +1,30 @@
 const TOKEN_KEY = 'token'
 const EXPIRES_AT_KEY = 'expiresAt'
+const storage = require('react-native-storage')
 
 export const getExpiresAt = () =>
-  Number(window.localStorage.getItem(EXPIRES_AT_KEY)) || null
+  Number(storage.getItem(EXPIRES_AT_KEY)) || null
 
 export const hasToken = () => getToken() !== null
 
 export const getToken = () => {
   const expiresAt = getExpiresAt()
   if (expiresAt === null || expiresAt > Date.now()) {
-    return window.localStorage.getItem(TOKEN_KEY) || null
+    return storage.getItem(TOKEN_KEY) || null
   }
   return null
 }
 
 export const setToken = (token, expiresAt) => {
-  window.localStorage.setItem(TOKEN_KEY, token)
+  storage.setItem(TOKEN_KEY, token)
   if (expiresAt !== null) {
-    window.localStorage.setItem(EXPIRES_AT_KEY, expiresAt)
+    storage.setItem(EXPIRES_AT_KEY, expiresAt)
   } else {
-    window.localStorage.removeItem(EXPIRES_AT_KEY)
+    storage.removeItem(EXPIRES_AT_KEY)
   }
 }
 
 export const removeToken = () => {
-  window.localStorage.removeItem(TOKEN_KEY)
-  window.localStorage.removeItem(EXPIRES_AT_KEY)
+  storage.removeItem(TOKEN_KEY)
+  storage.removeItem(EXPIRES_AT_KEY)
 }
